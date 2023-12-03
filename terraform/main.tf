167b1173996485e327cebb0fbc4251800f016f5d
acschwartz <31809318+acschwartz@users.noreply.github.com>
2023-12-02 21:09:05 -0600

    empty out main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+# tutorial to deploy dummy ec2 innstance
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-west-2 "
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
