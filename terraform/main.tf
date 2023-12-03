c240ad755097e7d874e1a7fe8c17678d13798193
joshua-Evans-1 <80655439+joshua-Evans-1@users.noreply.github.com>
2023-12-03 01:55:55 -0600

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+    required_providers {
+        google = {
+            source      = "hashicorp/google"
+            version     = "~> 4.51.0"
+        }
+        aws = {
+            source      = "hashicorp/aws"
+            version     = "~> 5.29.0"
+        }
+    }
+}
+
+provider "aws" {
+    region              = var.aws_region
+    profile             = var.aws_iAM
+    shared_credentials_files = [var.aws_cred_file]
+}
+
+provider "google" {
+    region              = var.gcp_region_name
+    credentials         = file(var.gcp_cred_file)
+    project             = var.gcp_project_id
+}
+
+module "gcp" {
+    source              = "./modules/gcp"
+    subnet_cidr         = var.gcp_subnet_cidr
+    network_name        = var.gcp_network_name
+}
+
+module "aws" {
+    source              = "./modules/aws"
+    vpc_cidr_block      = var.aws_vpc_cidr_block
+}
+/*
+module "vpn" {
+    source              = "./modules/vpn"
+    aws_vpc_id          = module.aws.vpc_id
+    aws_route_table_ids = [module.aws_cloud.public_route_table_id, module.aws_cloud.private_route_table_id]
+    gcp_network_id      = module.gcp.network_id
+}
+*/
