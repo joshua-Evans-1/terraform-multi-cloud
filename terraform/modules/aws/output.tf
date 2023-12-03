a2d0daba1114f6f03030a6e5d635b9e6b69cf710
joshua-Evans-1 <80655439+joshua-Evans-1@users.noreply.github.com>
2023-12-03 01:55:28 -0600

    Create output.tf

---
+++ terraform/modules/aws/output.tf
@@ -0,0 +1,35 @@
+output "vpc_id" {
+    value = aws_vpc.vpc_network.id
+}
+
+output "public_route_table_id" {
+    value = aws_route_table.public_route_table.id
+}
+
+output "private_route_table_id" {
+    value = aws_route_table.private_route_table.id
+}
+
+output "public_subnet_cidrs" {
+	value = aws_subnet.public_subnets.*.cidr_block 
+}
+
+
+output "public_subnets"{
+    value = aws_subnet.public_subnets.*.id
+}
+
+
+output "private_subnets" {
+    value = aws_subnet.private_subnets.*.id
+}
+
+
+output "private_subnet_cidrs"{
+    value = aws_subnet.private_subnets.*.cidr_block
+}
+
+
+output "nat_public_ip" {
+	value = aws_eip.nat_public_ip.public_ip 
+}
