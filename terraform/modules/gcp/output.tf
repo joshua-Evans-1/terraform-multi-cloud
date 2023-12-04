9033f896c6e8e0a057fe33e5b002b267503ca2b4
joshua-Evans-1 <80655439+joshua-Evans-1@users.noreply.github.com>
2023-12-03 23:58:13 -0600

    Update output.tf

--- terraform/modules/gcp/output.tf
+++ terraform/modules/gcp/output.tf
@@ -1,3 +1,7 @@
 output "network_id" {
     value = google_compute_network.project_network.id
 }
+
+output "nginx_ip" {
+    value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+}
